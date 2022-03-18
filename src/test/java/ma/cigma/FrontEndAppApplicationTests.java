c1fcf265f6086e472edd64c9be66b77f0cc2dfb6
mehdielorfi <mehdiorfi99@gmail.com>
2022-03-19 00:51:22 +0100

    first commit

---
+++ src/test/java/ma/cigma/FrontEndAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.cigma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontEndAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
